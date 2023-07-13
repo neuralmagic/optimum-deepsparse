1a2971dc0e05ab6b5cf97ab94fd6f99e613c16ae
Derrick Mwiti <mwitiderrick@gmail.com>
2023-07-13 15:22:59 +0300

    Update test_modeling.py

--- tests/deepsparse/test_modeling.py
+++ tests/deepsparse/test_modeling.py
@@ -724,12 +724,12 @@ class DeepSparseModelForTokenClassificationIntegrationTest(unittest.TestCase):
 
         model_info = self.ARCH_MODEL_MAP[model_arch] if model_arch in self.ARCH_MODEL_MAP else MODEL_DICT[model_arch]
         model_id = model_info.model_id
+        input_shapes = model_info.input_shapes
+        padding_kwargs = model_info.padding_kwargs
         # onnx_model = self.MODEL_CLASS.from_pretrained(self.onnx_model_dirs[model_arch])
-        onnx_model = self.MODEL_CLASS.from_pretrained(
-            model_id,
-            export=True,
-            # input_shapes=input_shapes
-        )
+        onnx_model = self.MODEL_CLASS.from_pretrained(model_id, export=True, 
+                                                      # input_shapes=input_shapes
+                                                     )
 
         self.assertIsInstance(onnx_model.config, PretrainedConfig)
 
@@ -740,7 +740,7 @@ class DeepSparseModelForTokenClassificationIntegrationTest(unittest.TestCase):
         text = "This is a sample output"
         tokens = tokenizer(text, return_tensors="pt")
         with torch.no_grad():
-            transformers_model(**tokens)
+            transformers_outputs = transformers_model(**tokens)
 
         for input_type in ["pt", "np"]:
             tokens = tokenizer(text, return_tensors=input_type)
@@ -763,12 +763,12 @@ class DeepSparseModelForTokenClassificationIntegrationTest(unittest.TestCase):
 
         model_info = self.ARCH_MODEL_MAP[model_arch] if model_arch in self.ARCH_MODEL_MAP else MODEL_DICT[model_arch]
         model_id = model_info.model_id
+        input_shapes = model_info.input_shapes
+        padding_kwargs = model_info.padding_kwargs
 
-        onnx_model = self.MODEL_CLASS.from_pretrained(
-            model_id,
-            export=True,
-            # input_shapes=input_shapes
-        )
+        onnx_model = self.MODEL_CLASS.from_pretrained(model_id, export=True,
+                                                      # input_shapes=input_shapes
+                                                     )
         tokenizer = get_preprocessor(model_id)
         pipe = pipeline("token-classification", model=onnx_model, tokenizer=tokenizer)
         text = "Norway is beautiful and has great hotels."
