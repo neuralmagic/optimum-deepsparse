4fc949d1301e145e21442d1fc24bf22acb45f5f6
Derrick Mwiti <mwitiderrick@gmail.com>
2023-07-18 14:08:53 +0000

    DeepSparseModelForMultipleChoice

--- optimum/deepsparse/__init__.py
+++ optimum/deepsparse/__init__.py
@@ -22,6 +22,7 @@ _import_structure = {
         "DeepSparseModel",
         "DeepSparseModelForSequenceClassification",
         "DeepSparseModelForImageClassification",
+        "DeepSparseModelForMultipleChoice",
     ],
 }
 
@@ -31,6 +32,7 @@ if TYPE_CHECKING:
         DeepSparseModel,
         DeepSparseModelForImageClassification,
         DeepSparseModelForSequenceClassification,
+        DeepSparseModelForMultipleChoice,
     )
 else:
     import sys
