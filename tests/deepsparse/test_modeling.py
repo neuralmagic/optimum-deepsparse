644ac4908149c255f632dcd2131227c03ef3754f
Derrick Mwiti <mwitiderrick@gmail.com>
2023-07-20 07:40:22 +0000

    test

--- tests/deepsparse/test_modeling.py
+++ tests/deepsparse/test_modeling.py
@@ -693,5 +693,3 @@ class DeepSparseModelForFeatureExtractionIntegrationTest(unittest.TestCase):
 
         # compare model output class
         self.assertTrue(all(all(isinstance(item, float) for item in row) for row in outputs[0]))
-
-        
