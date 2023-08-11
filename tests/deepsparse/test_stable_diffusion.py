6239903ed540446aa739bb3ae19945256c605a8e
Derrick Mwiti <mwitiderrick@gmail.com>
2023-08-11 17:52:33 +0300

    Update test_stable_diffusion.py

--- tests/deepsparse/test_stable_diffusion.py
+++ tests/deepsparse/test_stable_diffusion.py
@@ -22,10 +22,10 @@ from parameterized import parameterized
 from testing_utils import MODEL_DICT, SEED
 
 from optimum.deepsparse import (
-    # DeepSparseModelTextEncoder,
-    # DeepSparseModelUnet,
-    # DeepSparseModelVaeDecoder,
-    # DeepSparseModelVaeEncoder,
+    DeepSparseModelTextEncoder,
+    DeepSparseModelUnet,
+    DeepSparseModelVaeDecoder,
+    DeepSparseModelVaeEncoder,
     DeepSparseStableDiffusionPipeline,
 )
 
@@ -49,36 +49,11 @@ class DeepSparseStableDiffusionPipelineBaseTest(unittest.TestCase):
 
     MODEL_CLASS = DeepSparseStableDiffusionPipeline
 
-    # def generate_inputs(self, height=128, width=128):
-    #     inputs = _generate_inputs()
-    #     inputs["image"] = floats_tensor((1, 3, height, width), rng=random.Random(SEED))
-    #     inputs["strength"] = 0.75
-    #     return inputs
-
     def test_load_vanilla_model_which_is_not_supported(self):
         with self.assertRaises(Exception) as context:
             _ = self.MODEL_CLASS.from_pretrained(MODEL_DICT["bert"].model_id, export=True)
 
         self.assertIn(f"does not appear to have a file named {self.MODEL_CLASS.config_name}", str(context.exception))
-
-    # Option to overide batch_size not available at the moment
-    # @parameterized.expand(SUPPORTED_ARCHITECTURES)
-    # def test_num_images_per_prompt(self, model_arch: str):
-    #     model_info = self.ARCH_MODEL_MAP[model_arch] if model_arch in self.ARCH_MODEL_MAP else MODEL_DICT[model_arch]
-    #     model_id = model_info.model_id
-    #     num_images_per_prompt = 4
-    #     batch_size = 6
-    #     pipeline = self.MODEL_CLASS.from_pretrained(model_id, export=True)
-    #     self.assertEqual(pipeline.vae_scale_factor, 2)
-    #     self.assertEqual(pipeline.vae_decoder.config["latent_channels"], 4)
-    #     self.assertEqual(pipeline.unet.config["in_channels"], 4)
-    #     inputs = self.generate_inputs()
-    #     outputs = pipeline(**inputs).images
-    #     self.assertEqual(outputs.shape, (1, 128, 128, 3))
-    #     outputs = pipeline(**inputs, num_images_per_prompt=num_images_per_prompt).images
-    #     self.assertEqual(outputs.shape, (num_images_per_prompt, 128, 128, 3))
-    #     outputs = pipeline([inputs.pop("prompt")] * batch_size, **inputs).images
-    #     self.assertEqual(outputs.shape, (batch_size, 128, 128, 3))
 
     @parameterized.expand(SUPPORTED_ARCHITECTURES)
     def test_callback(self, model_arch: str):
@@ -112,50 +87,50 @@ class DeepSparseStableDiffusionPipelineTest(unittest.TestCase):
     FULL_GRID = {"model_arch": SUPPORTED_ARCHITECTURES}
     MODEL_CLASS = DeepSparseStableDiffusionPipeline
 
-    # @parameterized.expand(SUPPORTED_ARCHITECTURES)
-    # def test_compare_to_diffusers(self, model_arch: str):
-    #     model_info = self.ARCH_MODEL_MAP[model_arch] if model_arch in self.ARCH_MODEL_MAP else MODEL_DICT[model_arch]
-    #     model_id = model_info.model_id
-    #     pipeline = self.MODEL_CLASS.from_pretrained(model_id, export=True)
-    #     self.assertIsInstance(pipeline.text_encoder, DeepSparseModelTextEncoder)
-    #     self.assertIsInstance(pipeline.vae_encoder, DeepSparseModelVaeEncoder)
-    #     self.assertIsInstance(pipeline.vae_decoder, DeepSparseModelVaeDecoder)
-    #     self.assertIsInstance(pipeline.unet, DeepSparseModelUnet)
-    #     self.assertIsInstance(pipeline.config, Dict)
+    @parameterized.expand(SUPPORTED_ARCHITECTURES)
+    def test_compare_to_diffusers(self, model_arch: str):
+        model_info = self.ARCH_MODEL_MAP[model_arch] if model_arch in self.ARCH_MODEL_MAP else MODEL_DICT[model_arch]
+        model_id = model_info.model_id
+        pipeline = self.MODEL_CLASS.from_pretrained(model_id, export=True)
+        self.assertIsInstance(pipeline.text_encoder, DeepSparseModelTextEncoder)
+        self.assertIsInstance(pipeline.vae_encoder, DeepSparseModelVaeEncoder)
+        self.assertIsInstance(pipeline.vae_decoder, DeepSparseModelVaeDecoder)
+        self.assertIsInstance(pipeline.unet, DeepSparseModelUnet)
+        self.assertIsInstance(pipeline.config, Dict)
 
-    #     pipeline = StableDiffusionPipeline.from_pretrained(model_id)
-    #     pipeline.safety_checker = None
-    #     batch_size, num_images_per_prompt, height, width = 1, 2, 64, 64
+        pipeline = StableDiffusionPipeline.from_pretrained(model_id)
+        pipeline.safety_checker = None
+        batch_size, num_images_per_prompt, height, width = 1, 2, 64, 64
 
-    #     latents = pipeline.prepare_latents(
-    #         batch_size * num_images_per_prompt,
-    #         pipeline.unet.config["in_channels"],
-    #         height,
-    #         width,
-    #         dtype=np.float32,
-    #         generator=np.random.RandomState(SEED),
-    #         device="cpu",
-    #     )
+        latents = pipeline.prepare_latents(
+            batch_size * num_images_per_prompt,
+            pipeline.unet.config["in_channels"],
+            height,
+            width,
+            dtype=np.float32,
+            generator=np.random.RandomState(SEED),
+            device="cpu",
+        )
 
-    #     kwargs = {
-    #         "prompt": "sailing ship in storm by Leonardo da Vinci",
-    #         "num_inference_steps": 1,
-    #         "num_images_per_prompt": num_images_per_prompt,
-    #         "height": height,
-    #         "width": width,
-    #         "guidance_rescale": 0.1,
-    #     }
+        kwargs = {
+            "prompt": "sailing ship in storm by Leonardo da Vinci",
+            "num_inference_steps": 1,
+            "num_images_per_prompt": num_images_per_prompt,
+            "height": height,
+            "width": width,
+            "guidance_rescale": 0.1,
+        }
 
-    #     for output_type in ["latent", "np"]:
-    #         outputs = pipeline(latents=latents, output_type=output_type, **kwargs).images
-    #         self.assertIsInstance(outputs, np.ndarray)
-    #         with torch.no_grad():
-    #             outputs = pipeline(latents=torch.from_numpy(latents), output_type=output_type, **kwargs).images
-    #         # Compare model outputs
-    #         self.assertTrue(np.allclose(outputs, outputs, atol=1e-4))
+        for output_type in ["latent", "np"]:
+            outputs = pipeline(latents=latents, output_type=output_type, **kwargs).images
+            self.assertIsInstance(outputs, np.ndarray)
+            with torch.no_grad():
+                outputs = pipeline(latents=torch.from_numpy(latents), output_type=output_type, **kwargs).images
+            # Compare model outputs
+            self.assertTrue(np.allclose(outputs, outputs, atol=1e-4))
 
-    #     # Compare model devices
-    #     self.assertEqual(pipeline.device.type, pipeline.device)
+        # Compare model devices
+        self.assertEqual(pipeline.device.type, pipeline.device)
 
     @parameterized.expand(SUPPORTED_ARCHITECTURES)
     def test_image_reproducibility(self, model_arch: str):
@@ -173,24 +148,6 @@ class DeepSparseStableDiffusionPipelineTest(unittest.TestCase):
         self.assertTrue(np.array_equal(outputs_1.images[0], outputs_2.images[0]))
         self.assertFalse(np.array_equal(outputs_1.images[0], outputs_3.images[0]))
 
-    # @parameterized.expand(SUPPORTED_ARCHITECTURES)
-    # def test_num_images_per_prompt_static_model(self, model_arch: str):
-    #     model_info = self.ARCH_MODEL_MAP[model_arch] if model_arch in self.ARCH_MODEL_MAP else MODEL_DICT[model_arch]
-    #     model_id = model_info.model_id
-    #     pipeline = self.MODEL_CLASS.from_pretrained(model_id, export=True)
-    #     batch_size, num_images, height, width = 1, 4, 128, 64
-    #     prompt = "sailing ship in storm by Leonardo da Vinci"
-    #     # Verify output shapes requirements not matching the static model don't impact the final outputs
-    #     outputs = pipeline(
-    #         [prompt] * batch_size,
-    #         num_inference_steps=2,
-    #         num_images_per_prompt=num_images,
-    #         height=height,
-    #         width=width,
-    #         output_type="np",
-    #     ).images
-    #     self.assertEqual(outputs.shape, (batch_size * num_images, height, width, 3))
-
     @parameterized.expand(SUPPORTED_ARCHITECTURES)
     def test_height_width_properties(self, model_arch: str):
         model_info = self.ARCH_MODEL_MAP[model_arch] if model_arch in self.ARCH_MODEL_MAP else MODEL_DICT[model_arch]
@@ -206,5 +163,3 @@ class DeepSparseStableDiffusionPipelineTest(unittest.TestCase):
         image_height = outputs[0].size[1]
         self.assertEqual(image_width, width)
         self.assertEqual(image_height, height)
-        
-
